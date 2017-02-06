8782e609d9ad52ecbcf404a01eb8744e4406bf24
lukw33 <lukw34@gmail.com>
2017-02-06 21:36:42 +0100

    init pattern matching

--- src/main/java/uek/dev/pattern/matching/controllers/PatternMatching.java
+++ src/main/java/uek/dev/pattern/matching/controllers/PatternMatching.java
@@ -1,6 +1,7 @@
 package uek.dev.pattern.matching.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,16 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import uek.dev.pattern.matching.models.Entry;
+import uek.dev.pattern.matching.services.PatternMatcher;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/pattern")
-public class PatternMatcher {
+public class PatternMatching {
+
+    @Autowired
+    PatternMatcher morferPatternMatcher;
 
     @RequestMapping(value = "find", headers = "content-type=application/json", method = RequestMethod.POST)
     ResponseEntity<String> findPattern(@Valid @RequestBody  Entry entry) {
-        System.out.println(entry.getPattern());
+        morferPatternMatcher.match(entry.getSource(), entry.getPattern());
         return new ResponseEntity<String>("Done", HttpStatus.OK);
     }
 }
--- src/main/java/uek/dev/pattern/matching/services/MorferPatternMatcher.java
+++ src/main/java/uek/dev/pattern/matching/services/MorferPatternMatcher.java
@@ -1,18 +1,24 @@
 package uek.dev.pattern.matching.services;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import uek.dev.morfer.result.models.json.Word;
 import uek.dev.morfer.result.services.MorferService;
 import uek.dev.morfer.result.services.MorferServiceImpl;
 
 import java.util.ArrayList;
 
+@Service("morferPatternMatcher")
+@Transactional
 public class MorferPatternMatcher implements PatternMatcher {
     MorferService morferService = new MorferServiceImpl();
 
     @Override
     public boolean match(String entry, String pattern) {
-        ArrayList<String> analizedEntry = morferService.getMorferResult(entry);
-        ArrayList<String> analizedPattern = morferService.getMorferResult(entry);
-        return  false;
+        ArrayList<Word> analizedEntry = morferService.createModel(entry);
+        int entrySize = analizedEntry.size();
+
+        return true;
     }
 
     @Override
